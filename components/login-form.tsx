71b60b65ff4905223e85c65819190a08e5a1bf14
samassango <samassango@gmail.com>
2025-09-13 15:15:54 +0200

    implementing auto redirect when user authenticated

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -14,7 +14,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function LoginForm({
   className,
@@ -25,6 +25,16 @@ export function LoginForm({
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+
+  useEffect(() => {
+   const supabase = createClient();
+    supabase.auth.getClaims().then(({data})=>{
+      const user = data?.claims;
+      if(user){
+        router.push("/schedule");
+      }
+    })
+  }, [])
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
