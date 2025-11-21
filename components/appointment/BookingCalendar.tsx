b7de83ac255854f0c9bc86b3fcb5f70590d137fe
samassango <samassango@gmail.com>
2025-11-21 22:08:58 +0200

    implementing new design on appointment scheduling page

--- components/appointment/BookingCalendar.tsx
+++ components/appointment/BookingCalendar.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-big-calendar";
 import { format, parse, startOfWeek, getDay } from "date-fns"
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import './calendar.css';
 import { enUS } from "date-fns/locale";
 
 import { createClient } from "@/lib/supabase/client";
@@ -25,6 +26,16 @@ const localizer = dateFnsLocalizer({
 });
 
 type Appointment = Event;
+
+function sortByDate(events: Event[], order: 'asc' | 'desc' = 'asc'): Event[] {
+    return events.sort((a, b) => {
+        const dateA = new Date(a.start).getTime();
+        const dateB = new Date(b.start).getTime();
+
+        return order === 'asc' ? dateA - dateB : dateB - dateA;
+    });
+}
+
 
 const BookingCalendar = () => {
     const [appointments, setAppointments] = useState<Appointment[]>([]);
@@ -106,11 +117,13 @@ const BookingCalendar = () => {
 
     return (
         <div className="w-full px-4 sm:px-6 lg:px-8 py-6 max-w-7xl mx-auto">
-            <h1 className="text-xl sm:text-2xl md:text-3xl font-bold mb-6 text-center">
-                Appointment Booking
+            <h1 className="text-xl sm:text-2xl md:text-3xl  font-semibold text-gray-800 mb-6 text-center">
+                📅 Schedule Appointment
             </h1>
-
-            <div className="w-full h-[500px] sm:h-[650px] md:h-[600px] lg:h-[650px] xl:h-[800px] overflow-hidden">
+            {/* <h2 className="text-xl font-semibold mb-4 text-gray-800">
+                📅 Schedule Appointment
+            </h2> */}
+            <div className="calendar-container w-full h-[500px] sm:h-[650px] md:h-[600px] lg:h-[650px] xl:h-[650px] overflow-hidden">
                 <Calendar
                     localizer={localizer}
                     events={appointments}
@@ -120,7 +133,10 @@ const BookingCalendar = () => {
                     onSelectEvent={handleSelectEvent}
                     startAccessor="start"
                     endAccessor="end"
+                    // defaultDate={appointments.length ? appointments[0].start : null}
+                    // scrollToTime={appointments.length ? appointments[0].start : null}
                     style={{ height: '100%', width: '100%' }}
+                    className="custom-calendar"
                 />
             </div>
 
